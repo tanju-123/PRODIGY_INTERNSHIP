8c87d898ed5e084c2b1e1b239beb4a32c14f3d4e
Md Tanjil Hussain <btechtanjil15@gmail.com>
2025-06-20 23:18:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function () {
   const navbar = document.getElementById('navbar');
   if (window.scrollY > 50) {
-    navbar.style.backgroundColor = '#222';
+    navbar.classList.add('scrolled');
   } else {
-    navbar.style.backgroundColor = '#333';
+    navbar.classList.remove('scrolled');
   }
 });
